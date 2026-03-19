db4b4a62b28923f7bb5967f110e474b0ce1d7ac8
hyunjin <hyunjinking2323@gmail.com>
2026-03-19 22:58:48 +0900

    Repactory

--- MasterBall/MasterBall/ViewModel/DetailViewModel.swift
+++ MasterBall/MasterBall/ViewModel/DetailViewModel.swift
@@ -6,22 +6,42 @@ class DetailViewModel {
     private let disposeBag = DisposeBag()
     private let pokemon: Pokemon
 
-    let pokemonDetailRelay = BehaviorRelay<PokemonDetailResponse?>(value: nil)
-    let errorRelay = PublishRelay<Error>()
+        // VC와 소통할 규격 정의
+    struct Input {
+        let viewDidLoad: Observable<Void>
+    }
+
+    struct Output {
+        let pokemonDetail: Driver<PokemonDetailResponse?>
+        let errorMessage: Driver<String>
+    }
 
     init(pokemon: Pokemon) {
         self.pokemon = pokemon
     }
 
-    func fetchPokemonDetailRelay() {
-        guard let pokemonId = pokemon.id else { return }
-        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(pokemonId)/") else { return }
+    func transform(input: Input) -> Output {
+        let pokemonDetailRelay = BehaviorRelay<PokemonDetailResponse?>(value: nil)
+        let errorRelay = PublishRelay<String>()
 
-        NetworkManager.shared.fetch(url: url)
-            .subscribe(onSuccess: { [weak self] (detail: PokemonDetailResponse) in
-                self?.pokemonDetailRelay.accept(detail)
-            }, onFailure: { [weak self] error in
-                self?.errorRelay.accept(error)
-            }).disposed(by: disposeBag)
+        input.viewDidLoad
+            .flatMapFirst { [weak self] _ -> Observable<PokemonDetailResponse> in
+                guard let self = self, let pokemonId = self.pokemon.id,
+                      let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(pokemonId)/") else {
+                    return .empty()
+                }
+                return NetworkManager.shared.fetch(url: url).asObservable()
+            }
+            .subscribe(onNext: { detail in
+                pokemonDetailRelay.accept(detail)
+            }, onError: { error in
+                errorRelay.accept("포켓몬 정보를 불러오지 못했습니다.")
+            })
+            .disposed(by: disposeBag)
+
+        return Output(
+            pokemonDetail: pokemonDetailRelay.asDriver(onErrorJustReturn: nil),
+            errorMessage: errorRelay.asDriver(onErrorJustReturn: "알 수 없는 에러가 발생했습니다.")
+        )
     }
 }
